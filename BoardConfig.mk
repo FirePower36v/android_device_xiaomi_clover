1bec7ccf1af8942074208f982a3ad5d265a7421d
FirePower36v <75525177+FirePower36v@users.noreply.github.com>
2025-03-19 14:54:42 +0100

    Set screen density

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ $(call project-set-path,qcom-camera,$(DEVICE_PATH)/camera)
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchscreen/enable_dt2w"
 
 # Density
-TARGET_SCREEN_DENSITY := 320
+TARGET_SCREEN_DENSITY := 322
 
 # Kernel
 TARGET_KERNEL_CONFIG := clover_defconfig
